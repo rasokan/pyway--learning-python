5cacddf81cc2416ff2c0bd031ca6bebf949ec3d8
Vaquar Khan <vaquar.cna@gmail.com>
2018-07-29 01:20:07 -0500

    python 3.6

--- ex6.py
+++ ex6.py
@@ -7,25 +7,24 @@ do_not = "don't"
 # str in str 1st
 y = "Those who know %s and those who %s." % (binary, do_not)
 
-print x
-print y
+print (x)
+print(y)
 # str in str 2nd
-print "I said : %r." % x 
+print ("I said : %r." % x )
 # str in str 3rd 
-print "I also said :%r." % y
+print ("I also said :%r." % y)
 
 hilarious = False
 # str in str 4th 
 joke_evaluation = "Isn't that joke so funny?! %r"
 
-print "Isn't that joke so funny?! %r" % hilarious 
+print ("Isn't that joke so funny?! %r" % hilarious) 
 # equal to 
-print joke_evaluation % hilarious
+print (joke_evaluation % hilarious)
 
 w = "This is the left side of ..."
 e = "a string with a right side."
 
 # exlain ? 
 # ANS: maybe here the + is like a strlen function in 'C' 
-print w + e
-
+print( w + e)
