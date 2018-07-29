b98d7187ad987aad1385848dad74a25e7f43638e
Vaquar Khan <vaquar.cna@gmail.com>
2018-07-29 01:16:05 -0500

    pythin 3.6

--- ex5.py
+++ ex5.py
@@ -9,14 +9,12 @@ my_eyes = "Black"
 my_teech = "White"
 my_hair = "Black"
 
-print "Let's talk about %s." % my_name 
-print "He is %s cm tall." % my_height
-print "He is %s kilos heavy." % my_weight
-print "Actually that is not too heavy"
-print "He has got %s eyes and %s hair." % (my_eyes, my_hair) 
-print "His teech are usually %s depending on the coffee." % my_teech
+print("Let's talk about %s." % my_name ) 
+print ("He is %s cm tall." % my_height)
+print ("He is %s kilos heavy." % my_weight)
+print ("Actually that is not too heavy")
+print ("He has got %s eyes and %s hair." % (my_eyes, my_hair) )
+print ("His teech are usually %s depending on the coffee." % my_teech)
 
 # this line is tricky, try to get it exactly right 
-print "If I add %d, %d,and %d I get %d " % (my_height,my_weight,my_age, my_height+my_weight+my_age)
-
-
+print ("If I add %d, %d,and %d I get %d " % (my_height,my_weight,my_age, my_height+my_weight+my_age))
